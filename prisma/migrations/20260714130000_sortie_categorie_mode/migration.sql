f0d108c8ca9b5c5f16495d4018537a40ba1f2f66
PlumeDesChamps <tvcth2md7n@privaterelay.appleid.com>
2026-07-14 15:19:39 +0200

    Migrer les anciennes sorties vers la nouvelle fiche

---
+++ prisma/migrations/20260714130000_sortie_categorie_mode/migration.sql
@@ -0,0 +1,33 @@
+ALTER TABLE "Sortie" ADD COLUMN "categorieSortie" TEXT;
+ALTER TABLE "Sortie" ADD COLUMN "sexeSortie" TEXT;
+ALTER TABLE "Sortie" ADD COLUMN "modeVente" TEXT;
+ALTER TABLE "Sortie" ADD COLUMN "poidsVifVente" REAL;
+ALTER TABLE "Sortie" ADD COLUMN "prixKgVif" REAL;
+ALTER TABLE "Sortie" ADD COLUMN "poidsCarcasse" REAL;
+ALTER TABLE "Sortie" ADD COLUMN "prixKgCarcasse" REAL;
+
+UPDATE "Sortie"
+SET
+  "sexeSortie" = (SELECT "sexbov" FROM "Animal" WHERE "Animal"."id" = "Sortie"."animalId"),
+  "categorieSortie" = (
+    SELECT CASE
+      WHEN UPPER(COALESCE("categorie", '')) LIKE '%GENISSE%' THEN 'GENISSE'
+      WHEN UPPER(COALESCE("categorie", '')) LIKE '%VELLE%'
+        OR UPPER(COALESCE("categorie", '')) LIKE '%VEAU%' THEN 'VEAU'
+      WHEN UPPER(COALESCE("categorie", '')) LIKE '%TAUREAU%' THEN 'TAUREAU'
+      WHEN UPPER(COALESCE("categorie", '')) LIKE '%VACHE%' THEN 'VACHE'
+      WHEN "sexbov" = 'F' THEN 'VACHE'
+      ELSE 'VEAU'
+    END
+    FROM "Animal" WHERE "Animal"."id" = "Sortie"."animalId"
+  ),
+  "modeVente" = CASE
+    WHEN "type" = 'ELEVAGE' THEN 'VIF'
+    WHEN "type" = 'BOUCHERIE' THEN 'CARCASSE'
+    ELSE NULL
+  END,
+  "poidsVifVente" = CASE WHEN "type" = 'ELEVAGE' THEN "poids" ELSE NULL END,
+  "prixKgVif" = CASE WHEN "type" = 'ELEVAGE' THEN "prixKilo" ELSE NULL END,
+  "poidsCarcasse" = CASE WHEN "type" = 'BOUCHERIE' THEN "poids" ELSE NULL END,
+  "prixKgCarcasse" = CASE WHEN "type" = 'BOUCHERIE' THEN "prixKilo" ELSE NULL END;
+
