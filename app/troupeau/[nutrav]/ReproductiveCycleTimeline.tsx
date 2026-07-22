8748069ddccea7fbfa42c242cdc029a259ae96ab
quentin12200 <leyrat.quentin@gmail.com>
2026-07-22 09:27:43 +0200

    Corrige le transfert du composant

--- app/troupeau/[nutrav]/ReproductiveCycleTimeline.tsx
+++ app/troupeau/[nutrav]/ReproductiveCycleTimeline.tsx
@@ -1,6 +1,3 @@
-Exit code: 0
-Wall time: 0.6 seconds
-Output:
 "use client";
 
 import { useMemo, useState } from "react";
@@ -561,4 +558,3 @@ export default function ReproductiveCycleTimeline({
     </section>
   );
 }
-
