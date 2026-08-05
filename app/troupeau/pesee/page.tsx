c27c9a443fff46b6aa2e73e9837460a398230565
PlumeDesChamps <tvcth2md7n@privaterelay.appleid.com>
2026-08-05 17:27:05 +0200

    Affiche la date de la séance de pesée rapide

--- app/troupeau/pesee/page.tsx
+++ app/troupeau/pesee/page.tsx
@@ -1,10 +1,16 @@
 import type { Metadata } from "next";
 import FieldWeighingSession from "./FieldWeighingSession";
+import WeighingSessionDate from "./WeighingSessionDate";
 
 export const metadata: Metadata = {
   title: "Pesée rapide | CESAM",
 };
 
 export default function PeseeRapidePage() {
-  return <FieldWeighingSession />;
+  return (
+    <>
+      <FieldWeighingSession />
+      <WeighingSessionDate />
+    </>
+  );
 }
